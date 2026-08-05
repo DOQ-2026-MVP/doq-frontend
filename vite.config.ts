b3a7de806ffa7169e9f92cf5b67508ea35d46f2c
서지유 <jiyuseo2008@gmail.com>
2026-08-05 15:50:44 +0900

    feat :: 절대경로 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-})
+
+  resolve: {
+    alias: {
+      "@package": path.resolve(__dirname, "../../package"),
+      "@": path.resolve(__dirname, "src"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+});
