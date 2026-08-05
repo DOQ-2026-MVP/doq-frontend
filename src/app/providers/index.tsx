2699d37dd624c3d4de52d76fd626a0ddd989e980
서지유 <jiyuseo2008@gmail.com>
2026-08-05 15:52:01 +0900

    feat :: 클라이언트 및 라우터 프로바이더 세팅

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+type Props = {
+  router: ReturnType<typeof createBrowserRouter>;
+  client: QueryClient;
+};
+/**
+ * @description Providers component
+ * @param router - React Router
+ * @param client - React Query Client
+ */
+export const Providers = ({ router, client }: Props) => {
+  return (
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+
+      {import.meta.env.DEV && <ReactQueryDevtools />}
+    </QueryClientProvider>
+  );
+};
