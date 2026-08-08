12bb41a2dfc67a6381ca2290b71fd9ed2bcd2852
서지유 <jihoseo2008@gmail.com>
2026-08-08 23:25:39 +0900

    feat :: 프로바이더 추가

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,22 +1,26 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { RouterProvider } from "react-router"
+import { InspectionProvider } from "@/shared/context/InspectionContext"
 
 type Props = {
-  router: ReturnType<typeof createBrowserRouter>;
-  client: QueryClient;
-};
+    router: any
+    client: QueryClient
+}
+
 /**
  * @description Providers component
  * @param router - React Router
  * @param client - React Query Client
  */
 export const Providers = ({ router, client }: Props) => {
-  return (
-    <QueryClientProvider client={client}>
-      <RouterProvider router={router} />
+    return (
+        <QueryClientProvider client={client}>
+            <InspectionProvider>
+                <RouterProvider router={router} />
 
-      {import.meta.env.DEV && <ReactQueryDevtools />}
-    </QueryClientProvider>
-  );
-};
+                {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+            </InspectionProvider>
+        </QueryClientProvider>
+    )
+}
