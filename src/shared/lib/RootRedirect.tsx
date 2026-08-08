3e423dbe23a2549f71123bf75b375c043d51d56e
서지유 <jihoseo2008@gmail.com>
2026-08-08 23:37:03 +0900

    feat :: intake로

--- src/shared/lib/RootRedirect.tsx
+++ src/shared/lib/RootRedirect.tsx
@@ -1,13 +1,7 @@
-import { Navigate } from "react-router";
+import { Navigate } from "react-router"
 
-/**
- * @description 토큰 유무에 따른 리다이렉트 처리 (추후 refresh처리에 따라 api 기능 추가)
- */
 const RootRedirect = () => {
-  const token = localStorage.getItem("accessToken");
+    return <Navigate to={"/intake"} replace />
+}
 
-  // 지금은 무조건 home
-  return <Navigate to={token ? "/home" : "/home"} replace />;
-};
-
-export default RootRedirect;
+export default RootRedirect
